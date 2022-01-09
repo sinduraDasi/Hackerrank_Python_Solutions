9e8b69afb9b8a4cbafc8658a4ebed35dbfadc056
sindu1828 <84931658+sinduraDasi@users.noreply.github.com>
2022-01-09 13:21:12 +0530

    Update PythonIfElse.py

--- Introduction/PythonIfElse.py
+++ Introduction/PythonIfElse.py
@@ -2,18 +2,18 @@
 Task
 Given an integer, , perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Input Format
+If n  is odd, print Weird
+If n  is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird.
 
+Input Format
 A single line containing a positive integer, .
 
 Constraints
+1<n <100
 
 Output Format
-
 Print Weird if the number is weird. Otherwise, print Not Weird.
 -->
 
@@ -22,10 +22,25 @@ Print Weird if the number is weird. Otherwise, print Not Weird.
 
 
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
 import sys
 
-n = int(input())
-if n%2==1 or n in range (5,21):
-    print("Weird")
-else:
-    print("Not Weird")
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1 :
+        print("Weird")
+    elif n % 2 == 0 and 2 <= n <= 5 or n > 20  :
+        print("Not Weird")
+    elif n % 2 == 0 and 6 <= n <= 20:
+        print("Weird")
+    # elif n % 2 == 0 and  n > 20:
+    #     print("Not weird")  
+    else:
+        print("Weird")
