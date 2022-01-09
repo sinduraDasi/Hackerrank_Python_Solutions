c40a08fb0fcc48bf86bd0492abe6acaa945a98ff
sindu1828 <84931658+sinduraDasi@users.noreply.github.com>
2022-01-09 13:17:00 +0530

    Update PythonIfElse.py

--- Introduction/PythonIfElse.py
+++ Introduction/PythonIfElse.py
@@ -1,3 +1,27 @@
+<!
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+!>
+
+
+
+
+
+
 import sys
 
 n = int(input())
