6800c4e0d12a2779a82c10faf48135a9adb5b186
sindu1828 <84931658+sinduraDasi@users.noreply.github.com>
2022-01-11 19:06:20 +0530

    Create Arithmetic Operators.py

---
+++ Introduction/Intro/Arithmetic Operators.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+*********SOLUTION************************
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
